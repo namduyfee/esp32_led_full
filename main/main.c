b9c6035793bdf66656c6b7482fae9a2f88149c4f
Nam Duy <153448576+namduyfee@users.noreply.github.com>
2026-05-21 15:34:39 +0700

    fix version1

--- main/main.c
+++ main/main.c
@@ -12,7 +12,6 @@
 #include "main.h"
 #include "i2s_driver.h"
 
-void task_test_audio(void *param);
 void task_make_audio_signal(void *param);
 
 static const char *TAG = "MAIN";
@@ -44,64 +43,8 @@ void app_main(void)
     xAudioBufferList = xQueueCreate(10, sizeof(audio_buf_t));
 
     /** creat Task */
-    xTaskCreate(task_test_audio, "task_test_audio", 1024, NULL, 4, NULL);
     xTaskCreate(task_make_audio_signal, "task_make_audio_signal", 1024, NULL, 4, NULL);
     
-}
-
-#define MAX_SAMPLE 1024
-#define BYTE_OF_EACH_SAMP 2
-#define MAX_BYTE_OF_DATA (MAX_SAMPLE * BYTE_OF_EACH_SAMP)
-extern const uint8_t pcm_start[] asm("_binary_dlb_pcm_start");
-extern const uint8_t pcm_end[]   asm("_binary_dlb_pcm_end");
-
-/**
- * @brief   take audio data and send to task make audio signal
- */
-void task_test_audio(void *param)
-{
-    audio_buf_t buf_tem;
-
-    const uint8_t *pcm = pcm_start;
-    size_t pcm_size = pcm_end - pcm_start;
-    
-    int size_tem = pcm_size;
-
-    bool sent = false;
-    int16_t last_sample_value = 0;
-    while(1) {
-        while(size_tem > 0) {
-            size_t tot_byte = size_tem > MAX_BYTE_OF_DATA ? MAX_BYTE_OF_DATA : size_tem;
-            buf_tem.data = malloc(tot_byte);
-
-            if(buf_tem.data != NULL) {
-                buf_tem.tot_byte = tot_byte;
-                memcpy(buf_tem.data, pcm + pcm_size - size_tem, tot_byte);
-                if(xQueueSend(xAudioBufferList, &buf_tem, portMAX_DELAY) == pdTRUE) {
-                    size_tem -= tot_byte;
-                    memcpy(&last_sample_value, (uint8_t*)buf_tem.data + tot_byte - 2, 2); 
-                }
-            }
-            sent = true;
-            //vTaskDelay(pdMS_TO_TICKS(1));
-        }
-        if(sent == true) {
-            int16_t* fade_buf = malloc(64 * sizeof(int16_t));
-            for (int i = 0; i < 64; i++) {
-                fade_buf[i] = (int16_t)(last_sample_value * (64 - i) / 64);
-                printf("%d ", fade_buf[i]); 
-            }
-            
-            buf_tem.data = fade_buf;
-            buf_tem.tot_byte = 64 * sizeof(int16_t);
-            xQueueSend(xAudioBufferList, &buf_tem, portMAX_DELAY);
-            printf("last sample : %d\n", last_sample_value); 
-            size_tem = pcm_size;
-            sent = false;
-        }
-        
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
 }
 
 /**
